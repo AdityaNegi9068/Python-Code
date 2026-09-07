6e6c74c74050994d4003cd47860d5db33ea19a91
AdityaNegi9068 <adityanegi90689@gmail.com>
2026-09-07 19:33:48 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+"""
+Hey Aditya Negi, Please don't remove this line 
+Author: Aditya Negi
+today is day 2 of python coding
+"""
+print("HEY I AM A GOOD BOY\nand today is such a good day")
+
+print("hello world", 6,7, sep="~", end="\n009\n" )  # this is a comment
+print("Aditya Negi")
