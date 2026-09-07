508d8cea5c23c8549f2f43ecd27a9bc2fcea6613
AdityaNegi9068 <adityanegi90689@gmail.com>
2026-09-07 19:34:15 +0530

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,7 @@
+print(15 + 7) # Addition Operator
+print(15 - 7) # Subtraction Operator
+print(5 * 7) # Multiplication Operator
+print(5 ** 3) # Exponenntial Operator
+print(15/3) # Division Operator
+print(15//6) # Floor Division Operator
+print(5%3) # Modulo Operator
