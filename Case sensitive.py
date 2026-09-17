0a19cdaa6c98d04869b0d46103679f424b1df4b2
AdityaNegi9068 <adityanegi90689@gmail.com>
2026-09-17 11:15:54 +0530

    Add files via upload

---
+++ Case sensitive.py
@@ -0,0 +1,13 @@
+"""
+This program demonstrates the concept of case sensitivity in python.
+In Python variable names are case sensitive which means that "Name" and "name" are considered different variables.
+This program will show how using different case sensitivity can lead to diffrent outputs.
+"""
+name = "Aditya Negi" # This variable is defined with lowercase 'name'
+print(name) # This will print "Aditya Negi"
+Name = "Anuj Nehra" # This variable is defined with uppercase 'Name'
+print(Name) # This will print "Anuj Nehra"  
+'''
+Both 'name' and 'Name' are different variables due to case sensitivity, and they hold different values.
+This is an important concept to understand when programming in Python, as it can lead to unexpected results if not used carefully.
+'''
